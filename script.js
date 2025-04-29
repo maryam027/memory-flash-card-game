9f99892c8cf2c71fe33a2e5d54ee8ebec516c096
maryam027 <mariam.ali027@outlook.com>
2025-04-29 12:48:03 +0300

    Initialize game variables and emoji card list

---
+++ script.js
@@ -0,0 +1,9 @@
+const emojis = ["🍎", "🍎", "🥝", "🥝", "🥥", "🥥", "🍇", "🍇","🍊", "🍊","🍋","🍋"];
+let cards = [];
+let firstCard = null;
+let secondCard = null;
+let lockBoard = false;
+let matchCount = 0;
+let currentRound = 1;
+let timer;
+let seconds = 0;
